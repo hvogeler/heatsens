46665ac947bc84225145f7caecccec6040d1b663
Heiko Vogeler <hvogeler@abinitio.com>
2025-12-23 21:37:33 +0100

    usb

--- flash_nvs.sh
+++ flash_nvs.sh
@@ -3,4 +3,4 @@
 python $IDF_PATH/components/nvs_flash/nvs_partition_generator/nvs_partition_gen.py \
     generate nvs_creds.csv nvs_creds.bin 0x6000
 
-esptool.py -p /dev/cu.usbmodem201301 write_flash 0x9000 nvs_creds.bin
+esptool.py -p /dev/cu.usbmodem1301 write_flash 0x9000 nvs_creds.bin
